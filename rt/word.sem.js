8ccbea377743842ac749c6423364f193a2b5928f
Paul Tarvydas <guitarvydas@gmail.com>
2023-09-22 23:13:09 -0400

    break

--- rt/word.sem.js
+++ rt/word.sem.js
@@ -4,7 +4,10 @@
     word : function (legalchars) { return "❲" + legalchars.rwr ().join ('') + "❳"},
     separator: function (c) { return c.rwr () },
     comment: function (kcomment, cs, nl) { return kcomment.rwr () + cs.rwr ().join ('') + nl.rwr (); },
-    higherLevelSeparator: function (c) { return c.rwr () },
+    space : function (spc) { return spc.rwr (); },
+    string : function (dq1, cs, dq2) { return dq1.rwr () + cs.rwr ().join ('') + dq2.rwr (); },
+    dq : function (dq) { return dq.rwr (); },
+    extraSeparator: function (c) { return c.rwr () },
 
     _terminal: function() { return this.sourceString; },
     _iter: function (...children) { return children.map(c => c.rwr ()); }
